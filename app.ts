6ce8fa20730246aacfe31be21b41e3578c9e82a0
Ivan <forspamorelse@gmail.com>
2024-11-22 04:40:32 +0000

    Done

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log("Hello from Expat News!")
+import Feed from './src/FeedParser/Feed.js'
+
+const urls = [
+    'https://www.lapoliticaonline.com/files/rss/ultimasnoticias.xml',
+    'https://www.pagina12.com.ar/rss/portada'
+]
+
+function printFeed(feed: Feed) {
+    for(const note of feed) {
+        console.log(`${note}`)
+   }
+}
+
+for(const url of urls) {
+    const feed = await new Feed(url).update()
+    console.log(feed.ttl, feed.length)
+    feed.autoUpdate((update) => {
+        console.log('New updates')
+    })
+}
